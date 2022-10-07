1ee43078bf3ed93d13518a168e7adeaa83aa8de5
Rafl27 <raflsouza27@gmail.com>
2022-10-07 12:44:07 -0300

    basic next structure

---
+++ sanity_ecommerce/schemas/product.js
@@ -0,0 +1,41 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{ type: 'image' }],
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'name',
+            title: 'name',
+            type: 'string',
+        },
+        //slug is basically uuid
+        {
+            name: 'slug',
+            title: 'slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 90,
+            }
+        },
+        {
+            name: 'price',
+            title: 'price',
+            type: 'number',
+        },
+        {
+            name: 'details',
+            title: 'details',
+            type: 'string',
+        }
+    ]
+}
